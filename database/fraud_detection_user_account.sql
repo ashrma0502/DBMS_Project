a79797a28bcdefd5a772ddae9808edf086a93359
Aishwarya <aishwarya.pandey_cs25@gla.ac.in>
2026-01-11 21:22:34 +0530

    ADDING USER ACCOUNT DATBASE

---
+++ database/fraud_detection_user_account.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fraud_detection
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_account`
+--
+
+DROP TABLE IF EXISTS `user_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_account` (
+  `USERNAME` varchar(50) NOT NULL,
+  `ACCOUNT_NUMBER` bigint NOT NULL,
+  `PIN` int NOT NULL,
+  PRIMARY KEY (`ACCOUNT_NUMBER`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_account`
+--
+
+LOCK TABLES `user_account` WRITE;
+/*!40000 ALTER TABLE `user_account` DISABLE KEYS */;
+INSERT INTO `user_account` VALUES ('Vikram Patel',10233315300,9127),('Meera Iyer',12818461678,2089),('Siddharth Bansal',18225273503,2281),('Rahul Jain',20144155297,4077),('Dev Malhotra',37175172446,6522),('Aditya Deshmukh',38419952323,8229),('Karan Singh',41481692063,3958),('Sneha Kapoor',47186900382,5760),('Anjali Mishra',54695021112,6423),('Pooja Thakur',61186304852,7835),('Tanya Rao',65229972636,1511),('Sanya Choudhary',72754773916,2914),('Isha Nair',74187099921,7901),('Amit Verma',76375999481,6442),('Raghav Saxena',78674263846,5689),('Priya Reddy',79587391807,4892),('Nisha Kulkarni',79698522246,6014),('Ananya Gupta',85679082289,1370),('Arjun Sharma',87279735811,5738),('Rohan Mehta',98267854882,2920);
+/*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-01-11 21:17:35
